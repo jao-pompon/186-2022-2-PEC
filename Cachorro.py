a79f35a7b93fe293a3987f5b8a6c83ae1dd42613
jao-pompon <111912260+jao-pompon@users.noreply.github.com>
2022-08-24 13:19:50 -0300

    Add files via upload

---
+++ Cachorro.py
@@ -0,0 +1,8 @@
+print("Quantos anos você tem?")
+idade = input()
+idade = int(idade)
+cachorro = idade *7
+print("Se você fosse um cachorro, você teria", cachorro, "anos!!")
+print(" '0'_____' ")
+print('    || ||')
+      
